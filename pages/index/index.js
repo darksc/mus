a8d78ebe5de7f96f75465fde645fcd50b243879e
dark <123189717@qq.com>
2017-06-09 22:40:49 +0800

    添加商户选择页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,125 +1,66 @@
-//index.js
-var app = getApp()
-
+// index.js
 Page({
-  // 页面初始数据
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    latitude: 39.90403,
-    longitude: 116.407526,
-    controls: [],
-    markers: [],
-    // 列表数据
-    lists: [],
-    scrollInto: ''
+  
   },
-  // 生命周期函数--监听页面初次渲染完成
-  onReady () {
-    this.getLocation()
-    this.setControls()
-    this.getShops()
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
   },
-  // 滚动到底部/右边，会触发 scrolltolower 事件
-  scrolltolower (e) {
-    console.log(e)
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
   },
-  // 获取商家数据
-  getShops(params) {
-    var _this = this
-    wx.request({
-      url: app.globalData.url + '/shops',
-      data: {
-        x: '',
-        y: ''
-      },
-      header: {
-        'content-type': 'application/json'
-      },
-      success: function (res) {
-        if (res.statusCode == 200) {
-          _this.setData({
-            lists: res.data.data
-          })
-          var _data = res.data.data
-          var markers = []
-          for (var i = 0; i < _data.length; i++) {
-            markers.push({
-              id: _data[i].id,
-              latitude: _data[i].latitude,
-              longitude: _data[i].longitude,
-              iconPath: '/resources/Rating-48.png',
-              width: 20,
-              height: 20,
-              anchor: { x: .5, y: .5 }
-            })
-          }
-          _this.setLocation(markers)
-        }
-      }
-    })
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
   },
-  // 进入商家详情
-  listTab (e) {
-    wx.navigateTo({
-      url: '/pages/detail/detail?id=' + e.currentTarget.dataset.listid
-    })
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
   },
-  // 获取当前坐标点
-  getLocation () {
-    let _this = this
-    wx.getLocation({
-      type: 'gcj02 ',
-      success(res) {
-        _this.setData({
-          latitude: res.latitude,
-          longitude: res.longitude
-        })
-      }
-    })
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
   },
-  // 标记商家坐标点
-  setLocation (_markers) {
-    this.setData({
-      markers: _markers
-    })
-    
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
   },
-  // 地区变化事件处理
-  regionchange (e) {
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
   },
-  // 标记点点击事件处理
-  markertap (e) {
-    this.setData({
-      scrollInto: e.markerId
-    })
-  },
-  // 配置地图控件
-  setControls () {
-    let systemInfo = wx.getSystemInfoSync()
-    let width = systemInfo.windowWidth
-    let height = systemInfo.windowHeight
-    this.setData({
-      controls: [
-        {
-          id: 'loactionClick',
-          position: {
-            top: height * .4 - 58,
-            left: 10,
-            width: 48,
-            height: 48
-          },
-          iconPath: '/resources/Radar-48.png',
-          clickable: true
-        }
-      ]
-    })
-  },
-  // 控件点击事件处理
-  controltap (e) {
-    this[e.controlId]()
-  },
-  // 定位到我的位置
-  loactionClick () {
-    let map = wx.createMapContext('map')
-    map.moveToLocation()
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
   }
 })
---
+++ pages/list/list.js
@@ -0,0 +1,125 @@
+//index.js
+var app = getApp()
+
+Page({
+  // 页面初始数据
+  data: {
+    latitude: 39.90403,
+    longitude: 116.407526,
+    controls: [],
+    markers: [],
+    // 列表数据
+    lists: [],
+    scrollInto: ''
+  },
+  // 生命周期函数--监听页面初次渲染完成
+  onReady () {
+    this.getLocation()
+    this.setControls()
+    this.getShops()
+  },
+  // 滚动到底部/右边，会触发 scrolltolower 事件
+  scrolltolower (e) {
+    console.log(e)
+  },
+  // 获取商家数据
+  getShops(params) {
+    var _this = this
+    wx.request({
+      url: app.globalData.url + '/shops',
+      data: {
+        x: '',
+        y: ''
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        if (res.statusCode == 200) {
+          _this.setData({
+            lists: res.data.data
+          })
+          var _data = res.data.data
+          var markers = []
+          for (var i = 0; i < _data.length; i++) {
+            markers.push({
+              id: _data[i].id,
+              latitude: _data[i].latitude,
+              longitude: _data[i].longitude,
+              iconPath: '/resources/Rating-48.png',
+              width: 20,
+              height: 20,
+              anchor: { x: .5, y: .5 }
+            })
+          }
+          _this.setLocation(markers)
+        }
+      }
+    })
+  },
+  // 进入商家详情
+  listTab (e) {
+    wx.navigateTo({
+      url: '/pages/detail/detail?id=' + e.currentTarget.dataset.listid
+    })
+  },
+  // 获取当前坐标点
+  getLocation () {
+    let _this = this
+    wx.getLocation({
+      type: 'gcj02 ',
+      success(res) {
+        _this.setData({
+          latitude: res.latitude,
+          longitude: res.longitude
+        })
+      }
+    })
+  },
+  // 标记商家坐标点
+  setLocation (_markers) {
+    this.setData({
+      markers: _markers
+    })
+    
+  },
+  // 地区变化事件处理
+  regionchange (e) {
+  },
+  // 标记点点击事件处理
+  markertap (e) {
+    this.setData({
+      scrollInto: e.markerId
+    })
+  },
+  // 配置地图控件
+  setControls () {
+    let systemInfo = wx.getSystemInfoSync()
+    let width = systemInfo.windowWidth
+    let height = systemInfo.windowHeight
+    this.setData({
+      controls: [
+        {
+          id: 'loactionClick',
+          position: {
+            top: height * .4 - 38,
+            left: 10,
+            width: 28,
+            height: 28
+          },
+          iconPath: '/resources/location.png',
+          clickable: true
+        }
+      ]
+    })
+  },
+  // 控件点击事件处理
+  controltap (e) {
+    this[e.controlId]()
+  },
+  // 定位到我的位置
+  loactionClick () {
+    let map = wx.createMapContext('map')
+    map.moveToLocation()
+  }
+})
