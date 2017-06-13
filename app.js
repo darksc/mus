09c5ee6c3cabb4e9f505699fb75742fcfa5bd41e
dark <123189717@qq.com>
2017-06-13 12:25:38 +0800

    获取用户 openid~

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 //app.js
+const openIdUrl = require('./config').openIdUrl
+
 App({
   onLaunch: function () {
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
+
+    this.getUserOpenId((err, openId) => {
+      console.log(openId)
+    })
   },
   getUserInfo:function(cb){
     var that = this
@@ -26,6 +32,41 @@ App({
   },
   globalData:{
     userInfo:null,
-    url: 'https://www.easy-mock.com/mock/5937522591470c0ac106a9f1/mus'
+    url: 'https://www.easy-mock.com/mock/5937522591470c0ac106a9f1/mus',
+    hasLogin: false,
+    openid: null
+  },
+  // lazy loading openid
+  getUserOpenId: function (callback) {
+    var self = this
+
+    if (self.globalData.openid) {
+      callback(null, self.globalData.openid)
+    } else {
+      wx.login({
+        success: function (data) {
+          console.log(data)
+          wx.request({
+            url: openIdUrl,
+            data: {
+              code: data.code
+            },
+            success: function (res) {
+              console.log('拉取openid成功', res)
+              self.globalData.openid = res.data.openid
+              callback(null, self.globalData.openid)
+            },
+            fail: function (res) {
+              console.log('拉取用户openid失败，将无法正常使用开放接口等服务', res)
+              callback(res)
+            }
+          })
+        },
+        fail: function (err) {
+          console.log('wx.login 接口调用失败，将无法正常使用开放接口等服务', err)
+          callback(err)
+        }
+      })
+    }
   }
 })
